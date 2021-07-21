434ed2697f490898a05b2d986b0484296a849a78
Bhaskar Gogoi <bhaskargogoi24@gmail.com>
2021-07-21 04:22:47 +0200

    Updated Exceptions and Controller Advice

--- src/main/java/com/todo/advice/TodoControllerAdvice.java
+++ src/main/java/com/todo/advice/TodoControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.todo.advice;
 
 import com.todo.exception.InvalidConstraintException;
+import com.todo.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,6 +14,16 @@ public class TodoControllerAdvice {
     @ExceptionHandler(InvalidConstraintException.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid Todo Constraints...")
     public ResponseEntity<?> handleInvalidConstraintException(InvalidConstraintException ex) {
-        throw new InvalidConstraintException(ex.getMessage());
+        return makeResponseEntity(ex, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Todo Not found...")
+    public ResponseEntity<?> handleNotFoundException(NotFoundException ex) {
+        return makeResponseEntity(ex, HttpStatus.NOT_FOUND);
+    }
+
+    private static ResponseEntity<?> makeResponseEntity(RuntimeException ex, HttpStatus status) {
+        return ResponseEntity.status(status).body(ex.getMessage());
     }
 }
